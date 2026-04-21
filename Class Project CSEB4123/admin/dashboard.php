2b7bb7bc0f0d059cf054f7165b989544757c6b47
Jannah Razali <jannahjnh78@gmail.com>
2026-04-21 12:36:24 +0800

    login admin

--- Class Project CSEB4123/admin/dashboard.php
+++
@@ -1,5 +0,0 @@
-<html>
-    </html>44555 5555556556
-
-    test test
-    test again
---
+++ Class Project CSEB4123/admin/login.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+include("db_connect.php");
+
+if(isset($_POST['login'])){
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $query = "SELECT * FROM admin WHERE username='$username' AND password='$password'";
+    $result = mysqli_query($conn, $query);
+
+    if(mysqli_num_rows($result) == 1){
+        $_SESSION['admin'] = $username;
+        header("Location: admin_dashboard.php");
+        exit();
+    } else {
+        echo "Invalid username or password!";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Admin Login</title>
+</head>
+<body>
+
+<h2>Admin Login</h2>
+
+<form method="POST">
+    <input type="text" name="username" placeholder="Username" required><br><br>
+    <input type="password" name="password" placeholder="Password" required><br><br>
+    <button type="submit" name="login">Login</button>
+</form>
+
+</body>
+</html>
