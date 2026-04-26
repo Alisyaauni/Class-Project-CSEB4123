2af0db646c489fb7a2a18be6c270526a6ae49221
(❀ •̀ᴗ•́ )♡ pina <139848293+kieratempe@users.noreply.github.com>
2026-04-26 16:50:03 +0800

    remove welcome admin dekat search participants

--- Class Project CSEB4123/admin/search_admin.php
+++ Class Project CSEB4123/admin/search_admin.php
@@ -60,12 +60,11 @@ $result = mysqli_query($conn, $sql);
 <!-- MAIN CONTENT -->
 <div class="main">
 
- <div class="container mt-5">
-    <h2>Welcome, admin!</h2>
-</div>
+    <div class="topbar">
+        <h2>Search Participants</h2>
+    </div>
 
 <div style="margin-top: 20px;">
-    <h3>Search Participants</h3>
 
     <form method="GET">
         <input type="text" name="search" placeholder="Search by ID or Name" value="<?php echo $search; ?>">
